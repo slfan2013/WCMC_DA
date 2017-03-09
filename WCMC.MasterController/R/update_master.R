d18460d8d1ef257e30ebf311f57893fc2c433f55
slfan2013 <fansili2013@gmail.com>
2017-03-09 14:06:31 -0800

    a

--- WCMC.MasterController/R/update_master.R
+++ WCMC.MasterController/R/update_master.R
@@ -1,3 +1,3 @@
 update_master <- function() {
-  devtools::install_github("slfan2013/WCMC_DA/WCMC.Course2017.STAT",force=T)
+  devtools::install_github("slfan2013/WCMC_DA/WCMC.Course2017.STAT",force=F)
 }
