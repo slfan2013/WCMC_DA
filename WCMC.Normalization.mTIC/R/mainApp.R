2d3a5679b0d86041155d07dca5155e98c9940b9f
slfan2013 <fansili2013@gmail.com>
2017-03-21 10:38:16 -0700

    a

--- WCMC.Normalization.mTIC/R/mainApp.R
+++ WCMC.Normalization.mTIC/R/mainApp.R
@@ -15,7 +15,7 @@ mainApp = function(input){
   sums = apply(data[df1[,2]=='known',],2,  sum, na.rm=T)
   mean_sums = mean(sums)
   result = t(t(data)/(sums/mean_sums))
-  result = cbind(df1[,c(1,2)],result)
+  result = cbind(data.frame(compound = df1[,1]),result)
   rownames(result) = 1:nrow(result)
   fwrite(result,"mTIC-normalization.csv")
   fwrite(data.table(result),"mTIC-normalization.txt",sep = "\t")
