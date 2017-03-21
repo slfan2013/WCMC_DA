0fff6b5b696937d7e954416d8d07132f4e917b1a
slfan2013 <fansili2013@gmail.com>
2017-03-21 16:05:26 -0700

    a

--- WCMC.Course2017.STAT/R/launch_WCMC_DA.R
+++ WCMC.Course2017.STAT/R/launch_WCMC_DA.R
@@ -6,7 +6,6 @@
 # }
 
 update_childs <- function() {
-  install.packages("https://github.com/slfan2013/WCMC_DA/raw/master/WCMC.Course2017.STAT_0.1.0.tar.gz",repos=NULL)
   install.packages("https://github.com/slfan2013/WCMC_DA/raw/master/WCMC.Fansly_0.1.0.tar.gz",repos=NULL)
   install.packages("https://github.com/slfan2013/WCMC_DA/raw/master/WCMC.Normalization.mTIC_0.1.0.tar.gz",repos=NULL)
   install.packages("https://github.com/slfan2013/WCMC_DA/raw/master/WCMC.Normalization.SampleSpecific_0.1.0.tar.gz",repos=NULL)
