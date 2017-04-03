c91b19e22cc2fecde3dc3ac49c13b89c37ae9099
slfan2013 <fansili2013@gmail.com>
2017-04-03 14:42:29 -0700

    a

--- WCMC.Stat.OneWayRepANOVA/inst/www/WCMC.Stat.OneWayRepANOVA.js
+++ WCMC.Stat.OneWayRepANOVA/inst/www/WCMC.Stat.OneWayRepANOVA.js
@@ -25,7 +25,7 @@ $( "#codeToggle" ).click(function() {
 		.done(function(){
         $("#outputpanelheader").removeClass("collapsed")
         $("#output").addClass( "in" );
-        $("#output").html( "<b style='color:#3C763D;'>Success!</b><br /><div class='well well-sm'><p>NA</p></div><p style='color:grey';'>You can either view your normalized result <a href='"+view_address+"' target='_blank'>here</a>, or download your result by clicking the Download button.</p><a type='button' href='"+download_address+"' class='btn btn-primary' target='_blank'  download='OneWayRepANOVA.csv'>Download</a>" );
+        $("#output").html( "<b style='color:#3C763D;'>Success!</b><br /><div class='well well-sm'><p>NA</p></div><div class='well well-sm'><p><b>repANOVA p value:</b> the raw p value of one way repeated ANOVA.</p><p><b>pairwise-comparison: A_vs_B:</b> the post hoc analysis (paired t test with Holm (1979) FDR procedure) comparing A vs B.</p></div><p style='color:grey';'>You can either view your normalized result <a href='"+view_address+"' target='_blank'>here</a>, or download your result by clicking the Download button.</p><a type='button' href='"+download_address+"' class='btn btn-primary' target='_blank'  download='OneWayRepANOVA.csv'>Download</a>" );
 		})
 		.fail(function() {
 		  $('#output').empty();
--- WCMC.Stat.TwoWayMixedANOVA/inst/www/WCMC.Stat.TwoWayMixedANOVA.js
+++ WCMC.Stat.TwoWayMixedANOVA/inst/www/WCMC.Stat.TwoWayMixedANOVA.js
@@ -25,7 +25,7 @@ $( "#codeToggle" ).click(function() {
 		.done(function(){
         $("#outputpanelheader").removeClass("collapsed")
         $("#output").addClass( "in" );
-        $("#output").html( "<b style='color:#3C763D;'>Success!</b><br /><div class='well well-sm'><p>NA</p></div><p style='color:grey';'>You can either view your normalized result <a href='"+view_address+"' target='_blank'>here</a>, or download your result by clicking the Download button.</p><a type='button' href='"+download_address+"' class='btn btn-primary' target='_blank'  download='TwoWayMixedANOVA.csv'>Download</a>" );
+        $("#output").html( "<b style='color:#3C763D;'>Success!</b><br /><div class='well well-sm'><p>NA</p></div><div class='well well-sm'><p><b>A*B:</b> the raw p value of interaction effect of A and B, calculated by two-way ANOVA.</p><p><b>ANOVA (A):</b> the p value of the main effect of A, calculated by one-way Welch ANOVA.</p><p><b>ANOVA (A) @b:</b> the p value of the simple main effect of A at the level b of B, calculated by one-way Welch ANOVA.</p><p><b>a1 vs a2 @b:</b> the raw p value of the post hoc analysis of a1 vs a2 at the level b of B, calculated either by (paired) t test or Games-Howell test or paired t test with Holm (1997) FDR procedure depending on the number of levels and the dependency of A.</p><p><b>a1 vs a2 @b (FDR):</b> the adjusted p value of the raw p value (if available).</p></div><p style='color:grey';'>You can either view your normalized result <a href='"+view_address+"' target='_blank'>here</a>, or download your result by clicking the Download button.</p><a type='button' href='"+download_address+"' class='btn btn-primary' target='_blank'  download='TwoWayMixedANOVA.csv'>Download</a>" );
 		})
 		.fail(function() {
 		  $('#output').empty();
